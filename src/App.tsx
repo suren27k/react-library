55415fe6064d4acb71f4be0a00c6962e0a11fb01
Surendar K <surendar_new@hotmail.com>
2026-07-18 13:50:26 +0530

    init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Container } from "reactstrap";
+
+function App() {
+  return (
+    <Container className="vh-100 d-flex justify-content-center align-items-center">
+      <div>
+        <h1>React Library App</h1>
+      </div>
+    </Container>
+  );
+}
+
+export default App;
